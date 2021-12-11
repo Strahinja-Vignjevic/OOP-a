2b267553388a35a8fd3135781ceec2b2ba52eb73
Strahinja-Vignjevic <72220213+Strahinja-Vignjevic@users.noreply.github.com>
2021-12-11 16:46:43 +0100

    Update Stovariste.java

--- domaci3/Stovariste.java
+++ domaci3/Stovariste.java
@@ -1,7 +1,31 @@
 package Domaci3;
 
-public class Stovariste {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-	PolovnoVozilo nizVozila[];
-	
+public class Stovariste {
+    private PolovnoVozilo[] nizVozila;
+
+    Stovariste(String fajl) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fajl));
+        int brVozila = Integer.parseInt(br.readLine().trim());
+        nizVozila = new PolovnoVozilo[brVozila];
+
+        for (int i = 0; i < brVozila; i++) {
+            String linija = br.readLine().trim();
+            String[] delovi = linija.split(",");
+
+            String tipVozila = delovi[0];
+            int starost = Integer.parseInt(delovi[1]);
+            int treciBroj = Integer.parseInt(delovi[2]);
+
+            if (tipVozila.equals("auto")) {
+                nizVozila[i] = new PolovniAuto(starost, treciBroj);
+            } else {
+                nizVozila[i] = new PolovniKombi(starost, treciBroj);
+            }
+        }
+        br.close();
+    }
 }
